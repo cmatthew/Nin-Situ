2f8f4b9d7420c9171ca433a25126de41c163a032
Chris Matthews <chris4000@gmail.com>
2012-05-15 18:00:46 -0700

    started working on the TCP callbacks.

--- src/main.c
+++ src/main.c
@@ -6,61 +6,72 @@
 
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <ifaddrs.h>
-#include <errno.h>
-#include <netdb.h>
+/* Setttings */
+#define TCP_PORT 9999
+#define UDP_PORT 9999
+#define KNOWN_DNS_NAME "atmcc.trans-cloud.net"
+#define KNOWN_DNS_ADDRESS "1.2.3.4"
+#define SERVER_NAME "hypervisor-ilo.cs.uvic.ca"
 
-#define perror(x)  return make_result(FAILED, x,"")
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 enum test_status { NOT_DONE = 0, PASSED, WARNING, NO_RESULT, FAILED };
+
 
 char * status_name[] = {"Not Completed", "Passed", "Warning", "No Result", "Failed"};
 
 
 typedef struct test_result_s {
     enum test_status status;
-    char * problem;
-    char * description;
+    const char * problem;
+    const char * description;
 } result;
 
-void print_result(result * r) {
+
+static void print_result(result * r) {
     printf("%s: %s\n", status_name[r->status], r->problem);
     printf("%s\n", r->description);
     
 }
 
-result * get_blank_result() {
+
+static result * make_result( enum test_status tst, const char * problem, const char * description) {
     result * r = (result *) malloc(sizeof(result));
+    assert(r != NULL);
     memset(r, 0, sizeof(result));
-    return r;
-
-}
-
-result * make_result( enum test_status tst, char * problem, char * description) {
-    result * r = get_blank_result();
     r->status = tst;
+    r->problem = NULL;
     r->problem = problem;
     r->description = description;
     return r;
 }
 
+
+#define perror(x)  return make_result(FAILED, x,"")
+
+
+static void print_result(result * r) ;
+
 #define LINK_LOCAL_PREFIX "169.254."
 #define PRIVATE_PREFIX1 "10."
 #define PRIVATE_PREFIX3 "192.168."
 
-#define starts_with(x,y) strncmp(x, y, strlen(x)) == 0
+#define starts_with(x,y) (strncmp(x, y, strlen(x)) == 0)
 
 
 /** Check the condition of the local IP address.
@@ -68,7 +79,7 @@ result * make_result( enum test_status tst, char * problem, char * description) 
 Is is private, is it routable
 
 */
-result * test_ip_address() {
+static result * test_ip_address(void) {
     struct ifaddrs *myaddrs, *ifa;
     void *in_addr;
     char buf[64];
@@ -106,6 +117,7 @@ result * test_ip_address() {
             }
             // Are we a non-routable IP
             if ( starts_with(PRIVATE_PREFIX1, buf)) {
+                #warning "20-bit private addressing check is not implemented yet"
                 return make_result( PASSED, "Private non-routable IP address found", buf);
             }
             // Are we a private IP address
@@ -120,9 +132,9 @@ result * test_ip_address() {
 }
 
 
-result * test_dns_resolution() {
+static result * test_dns_resolution(void) {
 
-    struct hostent *hp = gethostbyname("atmcc.trans-cloud.net");
+    struct hostent *hp = gethostbyname(KNOWN_DNS_NAME);
 
     if (hp == NULL) {
         perror("DNS resolution failed");
@@ -130,12 +142,12 @@ result * test_dns_resolution() {
 
     struct in_addr* address = (struct in_addr *) hp -> h_addr_list[0];
     char * str_address = inet_ntoa( *( struct in_addr*) address );
-    if (strcmp(str_address, "1.2.3.4")==0){
-        return make_result(PASSED, "DNS resolution worked","query of atmcc."
-                           "trans-cloud.net returned the expected 1.2.3.4");
+    if (strcmp(str_address, KNOWN_DNS_ADDRESS)==0){
+        return make_result(PASSED, "DNS resolution worked","query of " KNOWN_DNS_NAME
+                           " returned the expected 1.2.3.4");
     } else {
         return make_result(FAILED, "DNS resolution failed",
-                           "query of atmcc.trans-cloud.net did not return"
+                           "query of " KNOWN_DNS_NAME " did not return"
                            " the expected 1.2.3.4");
     }
 
@@ -146,18 +158,20 @@ result * test_dns_resolution() {
 #define ECHO_MESSAGE "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 
-result * test_echo_server() {
+static result * test_echo_server(void) {
 
-    struct hostent     *he;
+    struct hostent     *he = NULL;
     struct sockaddr_in  server;
-    int sockfd = 0;
+    int sockfd = -1;
+
     memset(&server, 0, sizeof(server));
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     if (sockfd < 0) {
         perror("opening socket failed");
     }
     /* resolve localhost to an IP (should be 127.0.0.1) */
-    if ((he = gethostbyname("localhost")) == NULL) {
+    if ((he = gethostbyname(SERVER_NAME)) == NULL) {
         perror( "error resolving the hostname");
     }
 
@@ -167,7 +181,7 @@ result * test_echo_server() {
      */
     memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
     server.sin_family = AF_INET;
-    server.sin_port = htons(9999);
+    server.sin_port = htons(TCP_PORT);
     
     /* connect */
     if (connect(sockfd, (struct sockaddr *) &server, sizeof(server))) {
@@ -175,14 +189,17 @@ result * test_echo_server() {
     }
 
     int rc = write( sockfd, ECHO_MESSAGE, strlen(ECHO_MESSAGE)+1);
+
     if (rc != strlen(ECHO_MESSAGE) + 1) {
         return make_result(FAILED, "Could not send data to the server","");
     }
 
     char buf[65];
     memset(&buf, 0, 65);
-    int rc2 = read( sockfd, &buf, 65);
-    if (rc2 != strlen(ECHO_MESSAGE) + 1) {
+
+    int read_len = read( sockfd, &buf, 65);
+
+    if (read_len != strlen(ECHO_MESSAGE) + 1) {
         return make_result(FAILED, "Could not read data from the server","");
     }
 
@@ -191,18 +208,77 @@ result * test_echo_server() {
     } else {
         return make_result(FAILED, "TCP Echo test failed","Payload was not as expected");
     }
+
+}
+
+
+static result * test_tcp_callback(void) {
+
+    struct hostent     *he = NULL;
+    struct sockaddr_in  server;
+    int sockfd = -1;
+
+    memset(&server, 0, sizeof(server));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("opening socket failed");
+    }
+    /* resolve localhost to an IP (should be 127.0.0.1) */
+    if ((he = gethostbyname(SERVER_NAME)) == NULL) {
+        perror( "error resolving the hostname");
+    }
+
+    /*
+     * copy the network address part of the structure to the 
+     * sockaddr_in structure which is passed to connect() 
+     */
+    memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(TCP_PORT);
+    
+    /* connect */
+    if (connect(sockfd, (struct sockaddr *) &server, sizeof(server))) {
+        perror("error connecting to server");
+    }
+    
+    char * port = NULL;
+    (void) asprintf(&port, "%d", TCP_PORT + 1);
+    unsigned int rc = write( sockfd, port , strlen(port)+1);
+
+    if (rc != strlen(port) + 1) {
+        return make_result(FAILED, "Could not send data to the server","");
+    }
+
+    char buf[65];
+    memset(&buf, 0, 65);
+
+    int read_len = read( sockfd, &buf, 65);
+
+    if (read_len != strlen(ECHO_MESSAGE) + 1) {
+        return make_result(FAILED, "Could not read data from the server","");
+    }
+
+    if (strcmp(ECHO_MESSAGE, buf)==0) {
+        return make_result(PASSED, "TCP Echo test works","I message was relayed by the server");
+    } else {
+        return make_result(FAILED, "TCP Echo test failed","Payload was not as expected");
+    }
+
 }
 
 
 
-int main(int argc, char *argv[])
+int main(void)
 {
-
-    print_result(test_ip_address());
-
-    print_result(test_dns_resolution());
-
-    print_result(test_echo_server());
-
-    return 0;
+    result * r = NULL;
+    print_result(r = test_ip_address());
+    free(r);
+    print_result(r = test_dns_resolution());
+    free(r);
+    print_result(r = test_echo_server());
+    free(r);
+    print_result(r = test_tcp_callback());
+    free(r);
+    return EXIT_SUCCESS;
 }
--- src/nin-situ-server.py
+++ src/nin-situ-server.py
@@ -1,6 +1,6 @@
 import SocketServer
-
-
+import socket
+import string
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
     The RequestHandler class for our server.
@@ -11,7 +11,6 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
 
     def handle(self):
-        print ">>>>>>>>"
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         try:
@@ -19,12 +18,25 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         except:
             pass
         print self.data
+        try:
+            print "Data:", self.data
+            port = int(self.data.strip()[:-1])
+        except Exception, e:
+            print e
+            return
+        s = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
+        tup = (self.client_address[0], port)
+        print tup
+        s.connect(tup)
+        
+
         # just send back the same data
         self.request.sendall(self.data)
 
 if __name__ == "__main__":
     
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "", 9999
     print "Starting Sever on", HOST, ":", PORT
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
